ecb41216f5899eaaab83ea5ab19477d350c5884f
Ossman <carvajalossman@gmail.com>
2020-12-20 13:14:32 -0500

    Lista la encriptación de datos

--- db.py
+++ db.py
@@ -5,6 +5,8 @@ import sqlite3
 from sqlite3 import Error
 # acede a los valores de las variables enviadas por los HTML.
 from flask import Flask, request, render_template, redirect, url_for, current_app, g
+# importar el encriptador de datos
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 def get_db():
@@ -24,23 +26,36 @@ def close_db():
 
 
 def insertUsuarios(correo, contrasena):
+    hashed_password = generate_password_hash(contrasena)
     db = get_db()
     db.execute(
         "INSERT INTO usuarios (correo, contraseña) VALUES ('%s','%s')" % (
-            correo, contrasena)
+            correo, hashed_password)
     )
 
     db.commit()
     print("P2")
 
 
-def singUp(correo, contraseña):
+def auth_user(correo, contrasena):
     db = get_db()
-    res = db.execute(
-        "SELECT count(id) from usuarios where correo='%s' and contraseña = '%s'"  % (
-            correo, contraseña)
-    )
-    return res.fetchall()
+
+    res = db.execute( "SELECT count(id) from usuarios where correo='%s'"  % (correo) )
+
+    print(res)
+    res_fetch2 = res.fetchall()
+    
+    print(res_fetch2)
+
+    if res_fetch2[0][0] == 1:
+        res = db.execute("SELECT contraseña from usuarios where correo='%s'"  % (correo) )
+        res_fetch = res.fetchall()[0][0]
+        print(res_fetch)
+        print( check_password_hash(res_fetch, contrasena) )
+
+        return check_password_hash(res_fetch, contrasena)
+    else:
+        return False;
 
 
 
