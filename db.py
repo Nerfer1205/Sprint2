e1ac266d3964303c41bae89c76bde19e0ac4a34a
Daniel Torres <daniel_torres197@hotmail.com>
2020-12-20 14:01:23 -0500

    pycache actualizado

--- db.py
+++ db.py
@@ -5,6 +5,8 @@ import sqlite3
 from sqlite3 import Error
 # acede a los valores de las variables enviadas por los HTML.
 from flask import Flask, request, render_template, redirect, url_for, current_app, g
+# importar el encriptador de datos
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 def get_db():
@@ -23,24 +25,37 @@ def close_db():
         db.close()
 
 
-# def insertUsuarios(correo, contrasena):
-#     db = get_db()
-#     db.execute(
-#         "INSERT INTO usuarios (correo, contraseña) VALUES ('%s','%s')" % (
-#             correo, contrasena)
-#     )
+def insertUsuarios(correo, contrasena):
+    hashed_password = generate_password_hash(contrasena)
+    db = get_db()
+    db.execute(
+        "INSERT INTO usuarios (correo, contraseña) VALUES ('%s','%s')" % (
+            correo, hashed_password)
+    )
 
 #     db.commit()
 #     print("P2")
 
 
-# def singUp(correo, contraseña):
-#     db = get_db()
-#     res = db.execute(
-#         "SELECT count(id) from usuarios where correo='%s' and contraseña = '%s'"  % (
-#             correo, contraseña)
-#     )
-#     return res.fetchall()
+def auth_user(correo, contrasena):
+    db = get_db()
+
+    res = db.execute( "SELECT count(id) from usuarios where correo='%s'"  % (correo) )
+
+    print(res)
+    res_fetch2 = res.fetchall()
+    
+    print(res_fetch2)
+
+    if res_fetch2[0][0] == 1:
+        res = db.execute("SELECT contraseña from usuarios where correo='%s'"  % (correo) )
+        res_fetch = res.fetchall()[0][0]
+        print(res_fetch)
+        print( check_password_hash(res_fetch, contrasena) )
+
+        return check_password_hash(res_fetch, contrasena)
+    else:
+        return False;
 
 
 
